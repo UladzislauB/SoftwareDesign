14839a6ec3f76742dfa601ad45ed6110f4b21e4c
Uladzislau Vasiliuk <devuladblr@gmail.com>
2019-12-24 00:33:43 +0300

    finally found solution for invocation function in gradle script

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.TextView
+import android.provider.Settings.Secure
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pInfo = this.getPackageManager().getPackageInfo(packageName, 0)
+        val pInfo = this.packageManager.getPackageInfo(packageName, 0)
         val version = pInfo.versionName
 
-        val appVersionTxtView = findViewById<TextView>(R.id.appVersionTxtView) as TextView
+        val appVersionTxtView = findViewById<TextView>(R.id.appVersionTxtView)
         appVersionTxtView.text = version
+
+        val deviceIdTxtView = findViewById<TextView>(R.id.deviceIdTxtView)
+        deviceIdTxtView.text = Secure.getString(contentResolver, Secure.ANDROID_ID)
     }
 }
