2ed83dcf07303b6372b6fcb9a5a0f605c1851f4b
Uladzislau <devuladblr@gmail.com>
2020-01-20 18:37:01 +0300

    added NoteMainList ViewModel and ViewModelFactory

---
+++ Notes/app/src/main/java/com/example/notes/viewmodels/NoteMainListViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.notes.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.notes.dao.NoteDatabaseDAO
+import com.example.notes.models.Note
+import kotlinx.coroutines.*
+
+
+class NoteMainListViewModel(
+    dataSource: NoteDatabaseDAO,
+    application: Application
+) : ViewModel() {
+
+    val database = dataSource
+
+
+    // Coroutine variables
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    val notes = database.getAllNotes()
+
+
+    private val _navigateToNoteDetail = MutableLiveData<Note>()
+    val navigateToNoteDetail: LiveData<Note>
+        get() = _navigateToNoteDetail
+
+
+    // Call after navigating  to NoteDetailFragment
+    fun doneNavigating() {
+        _navigateToNoteDetail.value = null
+    }
+
+    // suspended functions
+    private suspend fun insert(note: Note) {
+        withContext(Dispatchers.IO) {
+            database.insert(note)
+        }
+    }
+
+
+    fun onCreate() {
+        uiScope.launch {
+            val note = Note()
+            insert(note)
+
+            // Navigating to NoteDetailFragment
+            _navigateToNoteDetail.value = note
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
---
+++ Notes/app/src/main/java/com/example/notes/viewmodels/NoteMainListViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.example.notes.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.notes.dao.NoteDatabaseDAO
+import java.lang.IllegalArgumentException
+
+class NoteMainListViewModelFactory(
+    private val dataSource: NoteDatabaseDAO,
+    private val application: Application
+) : ViewModelProvider.Factory {
+
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteMainListViewModel::class.java)) {
+            return NoteMainListViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
