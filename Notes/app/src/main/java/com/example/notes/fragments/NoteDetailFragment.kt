e4845b94df02a2fed5a18329b109382a0fc7ff2f
Uladzislau <devuladblr@gmail.com>
2020-01-22 23:55:24 +0300

    added clickHandler for color buttons

--- Notes/app/src/main/java/com/example/notes/fragments/NoteDetailFragment.kt
+++ Notes/app/src/main/java/com/example/notes/fragments/NoteDetailFragment.kt
@@ -56,7 +56,97 @@ class NoteDetailFragment : Fragment() {
         binding.viewModel = noteDetailViewModel
         binding.setLifecycleOwner(this)
 
+
+        // Setting color control handlers
+        binding.include.whiteColorBtn.setOnClickListener { colorBtnClickHandler(0) }
+        binding.include.redVioletBtn.setOnClickListener { colorBtnClickHandler(1) }
+        binding.include.teaRoseColorBtn.setOnClickListener { colorBtnClickHandler(2) }
+        binding.include.ochreColorBtn.setOnClickListener { colorBtnClickHandler(3) }
+        binding.include.orangeColorBtn.setOnClickListener { colorBtnClickHandler(4) }
+        binding.include.maizeColorBtn.setOnClickListener { colorBtnClickHandler(5) }
+        binding.include.electricLimeColorBtn.setOnClickListener { colorBtnClickHandler(6) }
+        binding.include.aquamarineColorBtn.setOnClickListener { colorBtnClickHandler(7) }
+        binding.include.skyBlueColorBtn.setOnClickListener { colorBtnClickHandler(8) }
+        binding.include.blueGreenColorBtn.setOnClickListener { colorBtnClickHandler(9) }
+        binding.include.lavenderColorBtn.setOnClickListener { colorBtnClickHandler(10) }
+        binding.include.peachColorBtn.setOnClickListener { colorBtnClickHandler(11) }
+        binding.include.silverColorBtn.setOnClickListener { colorBtnClickHandler(12) }
+
+
+
         return binding.root
+    }
+
+    fun colorBtnClickHandler(number: Int) {
+        binding.include.whiteColorBtn.text = ""
+        binding.include.redVioletBtn.text = ""
+        binding.include.teaRoseColorBtn.text = ""
+        binding.include.ochreColorBtn.text = ""
+        binding.include.orangeColorBtn.text = ""
+        binding.include.maizeColorBtn.text = ""
+        binding.include.electricLimeColorBtn.text = ""
+        binding.include.aquamarineColorBtn.text = ""
+        binding.include.skyBlueColorBtn.text = ""
+        binding.include.blueGreenColorBtn.text = ""
+        binding.include.lavenderColorBtn.text = ""
+        binding.include.peachColorBtn.text = ""
+        binding.include.silverColorBtn.text = ""
+
+
+        when (number) {
+            0 -> {
+                binding.include.whiteColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.white)
+            }
+            1 -> {
+                binding.include.redVioletBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.red_violet)
+            }
+            2 -> {
+                binding.include.teaRoseColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.tea_rose)
+            }
+            3 -> {
+                binding.include.ochreColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.ochre)
+            }
+            4 -> {
+                binding.include.orangeColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.orange)
+            }
+            5 -> {
+                binding.include.maizeColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.maize)
+            }
+            6 -> {
+                binding.include.electricLimeColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.electric_lime)
+            }
+            7 -> {
+                binding.include.aquamarineColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.aquamarine)
+            }
+            8 -> {
+                binding.include.skyBlueColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.sky_blue)
+            }
+            9 -> {
+                binding.include.blueGreenColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.blue_green)
+            }
+            10 -> {
+                binding.include.lavenderColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.lavender)
+            }
+            11 -> {
+                binding.include.peachColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.peach)
+            }
+            12 -> {
+                binding.include.silverColorBtn.text = "✔"
+                binding.noteDetailLayout.setBackgroundResource(R.color.silver)
+            }
+        }
     }
 
     override fun onPause() {
